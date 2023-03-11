fcb4243e729977736f568de2b659b7ea8acd7269
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-03-10 20:20:19 -0500

    posting to api creating user

--- api/server.js
+++ api/server.js
@@ -6,13 +6,34 @@ const server = express();
 //Global middleware
 server.use(express.json());
 
+server.post("/api/users", async (req, res) => {
+  const { name, bio } = req.body;
+  try {
+    if (!name || !bio) {
+      res.status(422).json({
+        message: "Name and Bio is needed to create user",
+      });
+    } else {
+      const createUser = await model.insert({ name, bio });
+      res.status(201).json({
+        message: "Success creating new user",
+        data: createUser,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: `Error creating new user: ${err.message}`,
+    });
+  }
+});
+
 server.get("/api/users", async (req, res) => {
   try {
     const allUsers = await model.find();
     res.status(200).json({ message: "Success fetching users", data: allUsers });
   } catch (err) {
     res.status(500).json({
-      message: "Error fetching users",
+      message: `Error fetching users: ${err.message}`,
     });
   }
 });
